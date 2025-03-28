ee993a39d33210fb63063aecb2dab4fffd8fbe31
tanakornsss <omtanakorn2550@gmail.com>
2025-03-28 10:48:26 +0700

    Finished the project.

--- c/order.c
+++ c/order.c
@@ -30,11 +30,22 @@
 int main()
 {
 	const int max_words = 4;
-	const char input[32];
-	const char output_buff[4][32];
 
-	int count = 0;
+	// This is a tax percent, represented as 7/100 = 0.07 = 7%
+	// We gonna be adding 1 later on, cause the total include tax will be 1.07 = 107%
+	const double tax_percent = 0.07;
+	
+	// Don't make the mistake that setting the char array TOO LOW.
+	// This may result it the text getting cutted of and ruin the program.
+	char input[100];
+
+	char output_buff[4][32];
 	char* token;
+
+	double total_no_tax = 0.0;
+	double total = 0.0;
+
+	int count =  0;
 	
 	// To compare with.
 	char order[5][32] = 
@@ -47,6 +58,9 @@ int main()
 
 	// To get strings include space.
 	fgets(input, sizeof(input), stdin);
+
+	// Remove \n out.
+	input[strcspn(input, "\n")] = 0;
 	
 	// For strtok() "Separating spaces."
 	token = strtok(input, " ");
@@ -61,11 +75,36 @@ int main()
 		// End copying strings.
 		token = strtok(NULL, " ");
 	}
-
-	for (int i = 0; i < count; i++)
+	
+	// The found int must be added, 
+	// or else it will stuck in loop and number will be messed up.
+	for (int i = 0; i < max_words; i++)
 	{
-		printf("Word %d: %s\n", i + 1, output_buff[i]);
+		int found = 0;
+
+		for (int j = 0; j < 5; j++)
+		{
+			// Use strcmp() to compare strings.
+			// Don't compare them directly output_buff[] is a pointer,
+			// but order[] IS NOT, so this if statement will be ignored.
+			if (strcmp(output_buff[i], order[j]) == 0)
+			{
+				found = 1;
+				total_no_tax += prices[j];
+				break;	
+			}
+		}
+
+		if (!found)
+		{	
+			total_no_tax += prices[4];	
+		}
 	}
+
+	total = total_no_tax * (1.0 + tax_percent);
+
+	// Make the output be the float in two digits.	
+	printf("%.2f\n", total);
 
 	return 0;
 }
