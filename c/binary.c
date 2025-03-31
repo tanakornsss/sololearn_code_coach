73a6123146da1611e22d755967f01f930c4b6c50
tanakornsss <omtanakorn2550@gmail.com>
2025-03-31 21:33:39 +0700

    Add binary.c

---
+++ c/binary.c
@@ -0,0 +1,43 @@
+// Number of Ones
+//
+//Task:
+// Count the number of ones in the binary representation of a given integer.
+//
+// Input Format:
+// An integer.
+//
+// Output Format: 
+// An integer representing the count of ones in the binary representation of the input.
+//
+// Sample Input:
+// 9
+//
+// Sample Output:
+// 2
+//
+// Explanation: 
+// The binary representation of 9 is 1001, which includes 2 ones.
+//
+// 
+// WTF is binary?
+// Binary is the another number format. It is base two number, unlike the usual base 10 numbers that we use in daily life.
+// Here's a simple conversion trick to convert for base ten numbers to base two.
+//
+// 	Base ten	->	Base two
+// 	0			0
+// 	1			1
+// 	2			10		(10^1)
+// 	3			11
+// 	4			100		(10^2)
+// 	5			101
+// 	6			110
+// 	7			111
+// 	8			1000		(10^3)
+// 	9			1001
+// 	.			.
+// 	.			.
+// 	.			.
+//
+// Starting to see the pattern now?
+
+
