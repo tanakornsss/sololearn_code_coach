ea4acd9b93992f9a9f78991b209c1bfa7903985b
tanakornsss <omtanakorn2550@gmail.com>
2025-04-01 21:31:09 +0700

    Add binary conversion. Results still reversed.

--- c/binary.c
+++ c/binary.c
@@ -39,5 +39,46 @@
 // 	.			.
 //
 // Starting to see the pattern now?
+//
+//
+// What if the nubmers are more than that?
+//
+// Ex. 45 to bianry.
+//
+// 	What we'll do is we will divide the number by two until it cannot be divided futher,
+// 	and take its remainder to be a bianry, by ordering from last to first.
+//
+// 	45 / 2 = 22 remainder 1     ^
+// 	22 / 2 = 11 "-------" 0     |
+// 	11 / 2 =  5 "-------" 1     |
+// 	 5 / 2 =  2 "-------" 1     |
+// 	 2 / 2 =  1 "-------" 0	    |
+//       1 / 2 =  0 "-------" 1	    | 	 (end it here, because the quotient is 0)
+//
+//       The binary form of 45 is 101101.
 
+
+#include <stdio.h>
+
+void toBinary(int base_ten)
+{
+	int binary_reversed = 0;
+
+	while (base_ten != 0) 
+	{
+		binary_reversed = base_ten % 2;
+		base_ten /= 2;
+		printf("%d", binary_reversed);		
+	}	
+}
+
+int main()
+{
+	int input = 0;
+
+	scanf("%d", &input);
+	toBinary(input);
+	
+	return 0;
+}
 
